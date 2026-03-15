84bf69ea85ac0bb0296a5d5847ecde83db1a3927
Nathan Hutton <nhutton110@gmail.com>
2026-03-15 18:58:54 -0500

    Fixed Error

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
 
 
